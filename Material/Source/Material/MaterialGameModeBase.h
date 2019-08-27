e5f5aab20b943da647f65b3295964898b8124462
earthboom <milkywaycrursh@gmail.com>
2019-08-28 01:00:25 +0900

    01:01 Material

---
+++ Material/Source/Material/MaterialGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MaterialGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATERIAL_API AMaterialGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
