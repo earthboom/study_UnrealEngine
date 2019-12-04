ddcecad5c0ee33a94f73920c6330345d2f71e454
earthboom <milkywaycrush@gmail.com>
2019-12-05 01:06:20 +0900

    Project Bellz Start

---
+++ Bellz/Source/Bellz/BellzGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BellzGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BELLZ_API ABellzGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
